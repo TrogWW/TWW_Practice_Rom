9350f5410fd9d730fa6c93b280cdb540a862308b
Dereck Miller <dereckjmiller1@gmail.com>
2021-04-14 14:14:22 -0400

    Screen capture for pause menu

--- TWW_Practice_Rom/menu.h
+++ TWW_Practice_Rom/menu.h
@@ -9,17 +9,30 @@
 #endif
 
 typedef struct Menu {
+    dDlst_MENU_CAPTURE_c* screen_capture;
     bool enabled;
     int toggle_menu;
     Settings* menu_settings;  
 } Menu;
 
 Menu menu = {
+    .screen_capture = 0,
     .enabled = false,
     .toggle_menu = 0,
     .menu_settings = &settings
 };
 void _menu_create(){
+   /* dDlst_MENU_CAPTURE_c** capture = dMs_capture_c;
+    capture = (dDlst_MENU_CAPTURE_c**)JKernel__operator_new(8);
+
+    *capture = (dDlst_MENU_CAPTURE_c*)&dDlst_base_c____vt;
+    *capture = &dDlst_MENU_CAPTURE_c____vt;
+    *(dDlst_MENU_CAPTURE_c*)(capture + 1) = 0;*/
+   
+    dDlst_MENU_CAPTURE_c* screen_capture = (dDlst_MENU_CAPTURE_c*)JKernel__operator_new(sizeof(dDlst_MENU_CAPTURE_c));
+    dDlst_MENU_CAPTURE_c__draw(screen_capture);
+
+    menu.screen_capture = screen_capture;
     d_meter__dMenu_flagSet(1);
     d_meter__dMenu_setMenuStatus(2);
     d_meter__dMenu_setMenuStatusOld();
@@ -28,6 +41,7 @@ void _menu_destroy(){
     d_meter__dMenu_flagSet(0);
     d_meter__dMenu_setMenuStatus(1);
     d_meter__dMenu_setMenuStatusOld();
+    dDlst_MENU_CAPTURE_c__dDlst_MENU_CAPTURE_c_destructor(menu.screen_capture);
 }
 void _menu_exec(Settings* settings){
     short currentInput = _input_current_digital();
@@ -48,6 +62,6 @@ void _menu_exec(Settings* settings){
         }
     }
     if(menu.enabled == true){
-        /*todo*/
+        dDlst_MENU_CAPTURE_c__draw(menu.screen_capture);
     }   
 }
